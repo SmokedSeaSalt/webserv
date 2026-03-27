c457d89b27f33afd101d5d6c3323eea44f7b3d14
SmokedSeaSalt <mathijsrijn@gmail.com>
2026-03-27 17:14:02 +0100

    start of HTTPMessage data

--- incl/parsing/parsing.hpp
+++ incl/parsing/parsing.hpp
@@ -1,8 +1,12 @@
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 struct HTTPMessage
 {
 	std::string buffer;
 	std::string method;
 	std::string version;
+	std::unordered_map<std::string, std::vector<std::string>> headers;
 };
+
