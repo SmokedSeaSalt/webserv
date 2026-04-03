bb96642528647948705fa32502bccc8e8429af25
SmokedSeaSalt <mathijsrijn@gmail.com>
2026-04-04 00:52:01 +0200

    finished first line validation

--- incl/parsing/HTTPRules.hpp
+++ incl/parsing/HTTPRules.hpp
@@ -28,9 +28,17 @@ class HTTPRules
         static auto is_field_content(unsigned char c) -> bool;
         static auto is_field_value(std::string value) -> bool;
 
+        static auto is_unreserved(unsigned char c) -> bool;
+        static auto is_sub_delims(unsigned char c) -> bool;
+        static auto is_hexdig(unsigned char c) -> bool;
+        static auto is_segment(std::string str) -> bool;
+
+        static auto is_absolute_path(std::string str) -> bool;
+        static auto is_query(std::string str) -> bool;
+        static auto is_origin_form(std::string str) -> bool;
+
         static auto validateMethod(std::string str) -> std::expected<bool, std::string>;
-        static auto validateRequestTarget(std::string str)
-            -> std::expected<std::string, std::string>;
+        static auto validateRequestTarget(std::string str) -> std::expected<bool, std::string>;
         static auto validateProtocol(std::string str) -> std::expected<bool, std::string>;
         static auto validateHeader(std::string key, std::string value) -> bool;
 
--- srcs/parsing/HTTPRules.cpp
+++ srcs/parsing/HTTPRules.cpp
@@ -36,6 +36,7 @@ auto HTTPRules::is_field_content(unsigned char c) -> bool
     return (is_vchar(c) || is_obs_text(c) || c == ' ' || c == '\t');
 }
 
+// TODO test when empty. should be valid
 auto HTTPRules::is_field_value(std::string value) -> bool
 {
     bool first    = is_field_vchar(*value.begin());
@@ -43,6 +44,116 @@ auto HTTPRules::is_field_value(std::string value) -> bool
     bool validKey = std::all_of(value.begin(), value.end(), HTTPRules::is_field_content);
 
     return (first && last && validKey);
+}
+
+auto HTTPRules::is_unreserved(unsigned char c) -> bool
+{
+    return (std::isalnum(c) || c == '-' || c == '.' || c == '_' || c == '~');
+}
+
+auto HTTPRules::is_sub_delims(unsigned char c) -> bool
+{
+    return (c == '!' || c == '$' || c == '&' || c == '\'' || c == '(' || c == ')' || c == '*' ||
+            c == '+' || c == ',' || c == ';' || c == '=');
+}
+
+auto HTTPRules::is_hexdig(unsigned char c) -> bool
+{
+    return (isdigit(c) || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F'));
+}
+
+// TODO test when empty. should be valid
+auto HTTPRules::is_segment(std::string str) -> bool
+{
+    int insidePctEncoded = 0;
+
+    for (char c : str)
+    {
+        if (insidePctEncoded > 0)
+        {
+            if (!is_hexdig(c))
+                ;
+            return false;
+            insidePctEncoded--;
+            continue;
+        }
+        if (c == '%')
+        {
+            insidePctEncoded = 2;
+            continue;
+        }
+        if (!(is_unreserved(c) || is_sub_delims(c) || c == ':' || c == '@'))
+            return false;
+    }
+    if (insidePctEncoded > 0)
+        return false;
+    return true;
+}
+
+// 1* ( "/" segment)
+auto HTTPRules::is_absolute_path(std::string str) -> bool
+{
+    if (str.empty() || str.front() != '/')
+        return false;
+
+    size_t pos = 0;
+    while (pos < str.size())
+    {
+        if (str[pos] != '/')
+            return false;
+
+        size_t      next    = str.find('/', pos + 1);
+        std::string segment = str.substr(pos + 1, next - (pos + 1));
+        if (!is_segment(segment))
+            return false;
+        pos = next;
+    }
+    return true;
+}
+
+auto HTTPRules::is_query(std::string str) -> bool
+{
+    int insidePctEncoded = 0;
+
+    for (char c : str)
+    {
+        if (insidePctEncoded > 0)
+        {
+            if (!is_hexdig(c))
+                ;
+            return false;
+            insidePctEncoded--;
+            continue;
+        }
+        if (c == '%')
+        {
+            insidePctEncoded = 2;
+            continue;
+        }
+        if (!(is_unreserved(c) || is_sub_delims(c) || c == ':' || c == '@' || c == '/' || c == '?'))
+            return false;
+    }
+    if (insidePctEncoded > 0)
+        return false;
+    return true;
+}
+
+auto HTTPRules::is_origin_form(std::string str) -> bool
+{
+    auto pos = str.find("?");
+    if (pos == std::string::npos)
+    {
+        if (is_absolute_path(str))
+            return true;
+        else
+            return false;
+    }
+
+    if (!is_absolute_path(str.substr(0, pos - 1)))
+        return false;
+    if (!is_query(str.substr(pos + 1, str.length() - (pos + 1))))
+        return false;
+    return true;
 }
 
 auto HTTPRules::validateMethod(std::string str) -> std::expected<bool, std::string>
@@ -53,20 +164,23 @@ auto HTTPRules::validateMethod(std::string str) -> std::expected<bool, std::stri
 }
 
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/Messages#request_targets
-auto HTTPRules::validateRequestTarget(std::string str) -> std::expected<std::string, std::string>
+auto HTTPRules::validateRequestTarget(std::string str) -> std::expected<bool, std::string>
 {
-    // TODO
+    if(!is_origin_form(str))
+        return std::unexpected("400 Bad Request");
+    return true;
 }
 
 auto HTTPRules::validateProtocol(std::string str) -> std::expected<bool, std::string>
 {
     if (str != HTTPVersion_)
         return std::unexpected("505 HTTP Version Not Supported");
+    return true;
 }
 
 auto HTTPRules::validateHeader(std::string key, std::string value) -> bool
 {
-    bool validKey = std::all_of(key.begin(), key.end(), HTTPRules::is_tchar);
+    bool validKey   = std::all_of(key.begin(), key.end(), HTTPRules::is_tchar);
     bool validValue = HTTPRules::is_field_value(value);
 
     return (validKey && validValue);
