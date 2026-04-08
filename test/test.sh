0c17f5a3de0700142becb9cb49dcaba763d1209e
SmokedSeaSalt <mathijsrijn@gmail.com>
2026-04-08 15:57:02 +0200

    add test script

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+# run_tests.sh
+# Run `make run` in multiple directories and exit immediately if any test fails
+
+
 NC='\e[0m' # No Color
 RED='\e[0;31m'
 GREEN='\e[0;32m'
@@ -8,11 +13,33 @@ WHITE='\e[1;37m'
 
 printf "%b\n" "${BLUE}Starting webserv tester...${NC}"
 
-mkdir -p incl && wget -q https://raw.githubusercontent.com/doctest/doctest/master/doctest/doctest.h -O incl/doctest.h && ls -lh incl/doctest.h
+set -e  # Exit immediately if any command fails
 
+# List of directories containing tests
+DIRS=(
+    "test/connection"
+    "test/parsing/config"
+    "test/parsing/HTTPRequest"
+)
 
-# Connection
-printf "%b\n" "${WHITE}Connection tests${NC}"
+for dir in "${DIRS[@]}"; do
+    echo "=== Running tests in $dir ==="
 
-make -C connection
-make -C parsing/config
+    if [ -d "$dir" ]; then
+        cd "$dir"
+        if ! make all >/dev/null 2>&1; then
+            echo "Make failed in $dir"
+            exit 1
+        fi
+		make run --no-print-directory
+        if ! make fclean >/dev/null 2>&1; then
+            echo "Make failed in $dir"
+            exit 1
+        fi
+        cd - > /dev/null
+    else
+        echo "Warning: directory $dir does not exist, skipping."
+    fi
+done
+
+printf "%b\n" "${GREEN}All tests complete!${NC}"
