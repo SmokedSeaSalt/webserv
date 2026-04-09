36901588737f5f329167f1d228208da70adff7aa
SmokedSeaSalt <mathijsrijn@gmail.com>
2026-04-09 17:18:51 +0200

    start of HTTPResponse

--- incl/parsing/HTTPResponse.hpp
+++ incl/parsing/HTTPResponse.hpp
@@ -6,6 +6,22 @@
 class HTTPResponse : public HTTPRules
 {
     public:
+        auto createPacket() -> std::string;
+        auto createErrorPacket(ResponseStatusCode) -> std::string;
+
+        auto setHeader(std::string key, std::string value);
+        auto addHeaderValue(std::string key, std::string value);
+        auto setBody(std::string data);
+
+    private:
+        HTTPMessage message_;
+
+        auto createFirstLine() -> std::string;
+        auto createHeaders() -> std::string;
+        auto createBody() -> std::string;
+
+        
+
 };
 
 #endif // HTTPRESPONSE
