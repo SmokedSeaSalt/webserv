568cb34b89686dd19450a3fd6de39289a9f35645
edsardgrisel <edsardgrisel1@gmail.com>
2026-04-15 16:05:28 +0200

    tests pass for execute for GET request

--- test/execution/non-CGI/main.cpp
+++ test/execution/non-CGI/main.cpp
@@ -1,6 +1,7 @@
 #include "Execution.hpp"
 #include "configParsing.hpp"
 #include "executionHelpers.hpp"
+#include "HTTPResponse.hpp"
 #include <filesystem>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../../incl/doctest.h"
@@ -13,25 +14,57 @@ TEST_CASE("Test readFile")
     CHECK(readFileResult.value() == "<p>Hello world</p>");
 }
 
-// TEST_CASE()
-// {
-//     auto configResult = parseConfigFile("config.conf");
-//     REQUIRE(configResult.has_value());
-//     Config    config = configResult.value();
-//     Execution execution(config);
+auto checkPacket(std::string packet) -> bool
+{
+    std::istringstream stream(packet);
+    std::string line;
+    bool foundStatus, foundServer, foundDate, foundBody = false;
 
-//     HTTPMessage httpMessage;
-//     httpMessage.method            = "GET";
-//     httpMessage.requestTarget     = "helloWorld.html";
-//     httpMessage.protocol          = "HTTP/1.1";
-//     httpMessage.headers["host"]   = {"localhost:8080"};
-//     httpMessage.headers["accept"] = {"text/html"};
-//     httpMessage.body              = "";
+    int lineNum = 0;
+    while (std::getline(stream, line, '\n'))
+    {
+        if (lineNum == 3)
+        {
+            lineNum++;
+            continue;
+        }
+        if (!line.empty() && line.back() == '\r')
+            line.pop_back();
 
-//     CHECK(httpMessage.method == "GET");
-//     CHECK(httpMessage.requestTarget == "helloWorld.html");
-//     CHECK(httpMessage.protocol == "HTTP/1.1");
+        if (lineNum == 0 && line == "HTTP/1.1 200 OK")
+            foundStatus = true;
+        if (lineNum == 1 && line.find("server: webserv") != std::string::npos)
+            foundServer = true;
+        if (lineNum == 2 && line.find("date: ") != 0)
+            foundDate = true;
+        if (lineNum == 4 && line == "<p>Hello world</p>")
+            foundBody = true;
+        lineNum++;
+    }
+    return foundStatus && foundServer && foundDate && foundBody;
+}
 
-//     HTTPResponse repsonse = execution.execute(httpMessage);
-//     // CHECK(repsonse. == "");
-// }
+// temp test case
+TEST_CASE("Hardcoded full path get test")
+{
+    auto configResult = parseConfigFile("config.conf");
+    REQUIRE(configResult.has_value());
+    Config    config = configResult.value();
+    Execution execution(config);
+
+    HTTPMessage httpMessage;
+    httpMessage.method            = "GET";
+    httpMessage.requestTarget     = "/home/egrisel/Repos/rank05/webserv_personal/test/execution/non-CGI/assets/helloWorld.html";
+    httpMessage.protocol          = "HTTP/1.1";
+    httpMessage.headers["host"]   = {"localhost:8080"};
+    httpMessage.headers["accept"] = {"text/html"};
+    httpMessage.body              = "";
+
+    CHECK(httpMessage.method == "GET");
+    CHECK(httpMessage.requestTarget == "/home/egrisel/Repos/rank05/webserv_personal/test/execution/non-CGI/assets/helloWorld.html");
+    CHECK(httpMessage.protocol == "HTTP/1.1");
+
+    HTTPResponse response = execution.execute(httpMessage);
+    CHECK(checkPacket(response.createPacket()));
+    // CHECK(repsonse. == "");
+}
