180c769b41b4f472e6b4d677f4275efd098015e4
SmokedSeaSalt <mathijsrijn@gmail.com>
2026-04-23 17:14:49 +0200

    format

--- incl/parsing/parsing.hpp
+++ incl/parsing/parsing.hpp
@@ -3,8 +3,8 @@
 
 #include <string>
 
-auto to_lower(std::string str) -> std::string;
-auto split(std::string line, char delimChar = ' ') -> std::expected<std::vector<std::string>, std::string>;
+auto        to_lower(std::string str) -> std::string;
+auto        split(std::string line, char delimChar = ' ') -> std::expected<std::vector<std::string>, std::string>;
 std::string stringTrim(const std::string& s);
 
 #endif
